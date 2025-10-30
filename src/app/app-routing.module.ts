3c87c535788afae7b2ad46086a1e39f63a10115e
namithaburnapelly <namitha.nja@gmail.com>
2025-10-30 18:40:59 +0530

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeGridComponent } from './home-grid/home-grid.component';
+import { WorkGridComponent } from './work-grid/work-grid.component';
+import { ProjectsGridComponent } from './projects-grid/projects-grid.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeGridComponent },
+  { path: 'work', component: WorkGridComponent },
+  { path: 'projects', component: ProjectsGridComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
